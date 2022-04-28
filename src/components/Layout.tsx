306cd9dad828c3bc16fa53d4ad266dcff3834fea
pawaox4th <pawaox4th@gmail.com>
2022-04-29 00:22:43 +0700

    Setup project.

---
+++ src/components/Layout.tsx
@@ -0,0 +1,7 @@
+import React, { HTMLAttributes, PropsWithChildren } from "react"
+
+function Layout({ children, className }: JSX.IntrinsicElements["div"]) {
+  return <div className={`w-6/12 m-auto ${className}`}>{children}</div>
+}
+
+export default Layout
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "montserrat": "Montserrat, sans-serif"
+      },
+      fontSize: {
+        "3xl": [
+          "30px",
+          {
+            "fontWeight": "bold"
+          }
+        ]
+      }
+    }
+  },
+  plugins: []
+}
+
